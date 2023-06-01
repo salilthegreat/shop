a06da1ccf394b2d8860c8008ead3c3c005ff378c
salilthegreat <salilkumardas22@gmail.com>
2023-06-01 08:09:55 -0700

    server created

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+const authRoute = require("./routes/auth")
+const userRoute = require("./routes/user")
+dotenv.config()
+
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("DB Connection Successful")
+}).catch((err)=>console.log(err))
+
+app.use("/api/auth",authRoute)
+app.use("/api/users",userRoute)
+
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("Backend Server is running!")
+})
