7a94b7407b4510a71d9ee8832dd6d179a2cdea52
salilthegreat <salilkumardas22@gmail.com>
2023-06-01 13:54:58 -0700

    api CRUD operation for products done

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv")
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/user")
+const productRoute = require("./routes/product")
 dotenv.config()
 
 app.use(express.json())
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
+app.use("/api/products",productRoute)
 
 app.listen(process.env.PORT || 5000,()=>{
     console.log("Backend Server is running!")
