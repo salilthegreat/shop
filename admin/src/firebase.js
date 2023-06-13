8ee651f38347a86c34cc66615420e981c2a3b81f
salilthegreat <salilkumardas22@gmail.com>
2023-06-13 06:53:31 -0700

    firebase upload image  and link generation enabled

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDohjPUz_fjg5mDWBzFqj0vKC0nRB8Qp5g",
+  authDomain: "shop-6b531.firebaseapp.com",
+  projectId: "shop-6b531",
+  storageBucket: "shop-6b531.appspot.com",
+  messagingSenderId: "142789494406",
+  appId: "1:142789494406:web:a6b6115c2c0a04bc8fb307"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- admin/src/pages/newProduct/NewProduct.jsx
+++ admin/src/pages/newProduct/NewProduct.jsx
@@ -1,52 +1,105 @@
 import { useState } from "react"
 import "./newProduct.css"
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import app from "../../firebase";
 
 export default function NewProduct() {
 
-    const[inputs,setInputs] = useState({});
-    const[file,setFile] = useState(null);
-    const[cat,setCat] = useState([])
+    const [inputs, setInputs] = useState({});
+    const [file, setFile] = useState(null);
+    const [cat, setCat] = useState([])
 
-    const handleChange = (e)=>{
-        setInputs((prev)=>{
-            return {...prev,[e.target.name]:e.target.value}
+    const handleChange = (e) => {
+        setInputs((prev) => {
+            return { ...prev, [e.target.name]: e.target.value }
         })
     }
-    const handleCat = (e)=>{
+    const handleCat = (e) => {
         setCat(e.target.value.split(","))
     }
-    console.log(cat)
+    console.log(file)
 
-    const handleClick = (e) =>{
+    const handleClick = (e) => {
         e.preventDefault();
-        
+        const fileName = new Date() + file.name
+        const storage = getStorage(app);
+        const storageRef = ref(storage, fileName);
+
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                    default:
+                }
+            },
+            (error) => {
+                // A full list of error codes is available at
+                // https://firebase.google.com/docs/storage/web/handle-errors
+                switch (error.code) {
+                    case 'storage/unauthorized':
+                        // User doesn't have permission to access the object
+                        break;
+                    case 'storage/canceled':
+                        // User canceled the upload
+                        break;
+
+                    // ...
+
+                    case 'storage/unknown':
+                        // Unknown error occurred, inspect error.serverResponse
+                        break;
+                    default:
+                }
+            },
+            () => {
+                // Upload completed successfully, now we can get the download URL
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    console.log('File available at', downloadURL);
+                    console.log({...inputs,img:downloadURL,categories:cat});
+                    // const product = { ...inputs, img: downloadURL, categories: cat };
+                });
+            }
+        );
+
+
     }
 
-  return (
-    <div className="newProduct">
-                    <h1 className="newProductTitle">New Product</h1>
+    return (
+        <div className="newProduct">
+            <h1 className="newProductTitle">New Product</h1>
             <form className="newProductForm">
 
-            <div className="newProductItem">
+                <div className="newProductItem">
                     <label >Image</label>
-                    <input type="file" id="file" onChange={(e)=>setFile(e.target.files[0])} />
+                    <input type="file" id="file" onChange={(e) => setFile(e.target.files[0])} />
                 </div>
 
                 <div className="newProductItem">
                     <label >Title</label>
-                    <input name="title" type="text" placeholder="Apple Airpods" onChange={handleChange}/>
+                    <input name="title" type="text" placeholder="Apple Airpods" onChange={handleChange} />
                 </div>
                 <div className="newProductItem">
                     <label >Price</label>
-                    <input name="price" type="number" placeholder="100" onChange={handleChange}/>
+                    <input name="price" type="number" placeholder="100" onChange={handleChange} />
                 </div>
                 <div className="newProductItem">
                     <label >Categories</label>
-                    <input type="text" placeholder="jeans,skirt" onChange={handleCat}/>
+                    <input type="text" placeholder="jeans,skirt" onChange={handleCat} />
                 </div>
                 <div className="newProductItem">
                     <label >Description</label>
-                    <input name="desc" type="text" placeholder="description..." onChange={handleChange}/>
+                    <input name="desc" type="text" placeholder="description..." onChange={handleChange} />
                 </div>
                 <div className="newProductItem">
                     <label >Stock</label>
@@ -57,6 +110,6 @@ export default function NewProduct() {
                 </div>
                 <button onClick={handleClick} className="newProductButton">Create</button>
             </form>
-    </div>
-  )
+        </div>
+    )
 }
